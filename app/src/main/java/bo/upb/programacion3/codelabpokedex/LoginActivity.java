61621dc4b56fe37154a54e8372524daa8ceff69c
Luis Fer <pastillalc@gmail.com>
2019-12-03 12:13:32 -0400

    Log in First pass

---
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/LoginActivity.java
@@ -0,0 +1,73 @@
+package bo.upb.programacion3.codelabpokedex;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private Context context;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button registerButton;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+        this.context = this;
+        //initUI();
+        //initEvents();
+
+        emailEditText.setText("cualquiera@pumatiti.com");
+        passwordEditText.setText("noImporta");
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("message")) {
+            String message = intent.getStringExtra("message");
+            Toast.makeText(this, // Context, origen
+                    message, //Mensaje
+                    Toast.LENGTH_SHORT) //Duración
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
